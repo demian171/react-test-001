b9d4414716998b57e6d4d9b21ecbadba9c8f348c
Dmytro Bilyak <demian171@gmail.com>
2022-07-27 15:17:47 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Footer() {
+    return (
+        <div className="header">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+
+            </header>
+        </div>
+    );
+}
+
+export default Footer;
